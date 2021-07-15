1d0f86cf29805b3120f5e14d44eeb4a569eae143
tonhathuy <tonhathuy97@gmail.com>
2021-07-15 16:15:54 +0700

    add navbar

--- three_3d/src/components/navbar.jsx
+++ three_3d/src/components/navbar.jsx
@@ -3,36 +3,52 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import ReactDatGui from "./ReactDatGui";
 
 import Three_test_2 from "./three_test_2";
 
 
 const Body_ = (props) => {
-        return (
-        <div id="webgl">
-        <Three_test_2 geometry={props.geometry} surface={props.surface} light={props.light} animation={props.animation}/>
-        </div>
-        )
-    }
+    
+    
+    return (
+    <>
+    <ReactDatGui initialDatState={props.data} geometry={props.geometry} handleUpdate={props.handleUpdate}/>
+    <div id="webgl" style={{position: "absolute"}}>
+    <Three_test_2 data={props.data} geometry={props.geometry} surface={props.surface} light={props.light} animation={props.animation}/>
+    </div>
+    </>
+    )
+}
 
 
 class Navbar_ extends React.Component{
     constructor(props){
         super(props);
+        const initialDatState = {
+            string: "Camera",
+            far: 66,
+            fov: 66,
+            near: 0.2,
+            colorObject: "#2FA1D6",
+            lightPosition: 2,
+        };
         this.state = { 
             geometry: "box",
-            surface: "solid",
+            surface: "default",
             light: "none",
-            animation: "none"
+            animation: "none",
+            data: initialDatState,
         };
+        
         this.ref = React.createRef();
 
     }
 
-    handleButton = () => {
-            this.ref.current.focus();
-    };
+    handleUpdate = newData =>
+    this.setState(prevState => ({
+        data: { ...prevState.data, ...newData }
+    }));
     render(){
         return (
             <>
@@ -53,6 +69,7 @@ class Navbar_ extends React.Component{
                         <NavDropdown.Item onClick={() => {this.setState({surface:"point"})}}>point</NavDropdown.Item>
                         <NavDropdown.Item onClick={() => {this.setState({surface:"line"})}}>line</NavDropdown.Item>
                         <NavDropdown.Item onClick={() => {this.setState({surface:"solid"})}}>solid</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => {this.setState({surface:"default"})}}>default</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Light" id="collasible-nav-dropdown">
                         <p>Value {this.state.light}</p>
@@ -66,11 +83,12 @@ class Navbar_ extends React.Component{
                         <NavDropdown.Item onClick={() => {this.setState({animation:"box"})}}>sphere</NavDropdown.Item>
                         <NavDropdown.Item onClick={() => {this.setState({animation:"box"})}}>cone</NavDropdown.Item>
                     </NavDropdown>
+                    <p>{this.state.data.string}</p>
                 </Nav>
                 
             </Navbar.Collapse>
             </Navbar>
-            <Body_ geometry={this.state.geometry} surface={this.state.surface} light={this.state.light} animation={this.state.animation}/>
+            <Body_ geometry={this.state.geometry} surface={this.state.surface} light={this.state.light} animation={this.state.animation} data={this.state.data} handleUpdate={this.handleUpdate}/>
             </>
         );
     }
--- three_3d/src/components/three_test_2.jsx
+++ three_3d/src/components/three_test_2.jsx
@@ -1,30 +1,94 @@
 import React, { Component } from "react";
 import * as THREE from "three";
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import  TransformControls  from '../source/TransformControls.js'
+import fileGlb from './bread.glb' // GLB FILE
+
+
+function getPlane(size) {
+	var geometry = new THREE.PlaneGeometry(size, size);
+	var material = new THREE.MeshPhongMaterial({
+		color: 'rgb(120, 120, 120)',
+		side: THREE.DoubleSide
+	});
+	var mesh = new THREE.Mesh(
+		geometry,
+		material 
+	);
+
+	return mesh;
+}
+function getSphere(size) {
+	var geometry = new THREE.SphereGeometry(size, 24, 24);
+	var material = new THREE.MeshBasicMaterial({
+		color: 'rgb(255, 255, 255)'
+	});
+	var mesh = new THREE.Mesh(
+		geometry,
+		material 
+	);
+
+	return mesh;
+}
 
 class Scene extends Component {
     constructor(props) {
         super(props);
-
+        this.OrbitControls = require('three-orbit-controls')(THREE)
         this.start = this.start.bind(this);
         this.stop = this.stop.bind(this);
         this.animate = this.animate.bind(this);
+        this.state = {position: { x: 0, y: 0, z: 0 },
+                rotation: { x: 0, y: 0, z: 0 },
+                scale: { x: 3, y: 3, z: 3 } }
     }
+    // FUNCTION FOR SAVE VALUES FORM INPUTS
+    stateUpdate = (event, direction, axis, ajustFunc) => {
+        // CHANGING EVENT TO MOUSE DATA IF...
+        const targetValue = typeof event === "number" ? event : event.target.value;
+        const isNum = targetValue === "" ? "" : Number(targetValue);
+        this.setState(state => {
+        return state[direction] = { ...state[direction], [axis]: isNum }
+        }, () => ajustFunc(direction, axis));
+    };
 
     componentDidMount() {
         const width = this.mount.clientWidth;
         const height = this.mount.clientHeight;
-
+        this.OrbitControls = require('three-orbit-controls')(THREE)
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+        // const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+        var camera = new THREE.PerspectiveCamera(
+            45,
+            width/height,
+            1,
+            1000
+        );
         const renderer = new THREE.WebGLRenderer({ antialias: true });
         const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: "#433F81" });
+        const material = new THREE.MeshBasicMaterial({ color: this.props.data.colorObject });
+        var plane = getPlane(20);
         const cube = new THREE.Mesh(geometry, material);
+
+        const size = 10;
+        const divisions = 25;
+        // GRID helper
+        this.gridHelper = new THREE.GridHelper( size, divisions );
+        cube.position.y = cube.geometry.parameters.height/2;
+        plane.rotation.x = Math.PI/2;
         const light = new THREE.PointLight( 0xffffff, 1);
         light.position.y = 1;
-        camera.position.z = 4;
+        camera.position.x = 1;
+        camera.position.y = 2;
+        camera.position.z = 5;
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+        
+        scene.fog = new THREE.FogExp2(0xffffff, 0.2);
         scene.add(cube);
         scene.add(light);
+        scene.add(plane);
+        scene.add( this.gridHelper );
         renderer.setClearColor("#000000");
         renderer.setSize(width, height);
 
@@ -34,14 +98,35 @@ class Scene extends Component {
         this.material = material;
         this.cube = cube;
         
+        // var TransformControls = require('../controls/TransformControls')(THREE);
+        this.control = new TransformControls( this.camera, this.renderer.domElement );
+        this.control.addEventListener( 'change', () => { this.renderer.render(this.scene, this.camera); });
+        this.control.setSize(1);
+        this.control.setMode( "translate" );
+        this.controls = new this.OrbitControls(this.camera);
         this.mount.appendChild(this.renderer.domElement);
         this.start();
     }
+      // UPDATE REACT STATE AND INPUT VALUE
+    updateSetState = () => {
+        if (this.gltf !== undefined) {
+        this.setState((state) => {
+            const gltf = this.gltf[this.valType.value]
+            return state[this.valType.value] = {x:gltf.x, y:gltf.y, z:gltf.z,}
+        });
+        };
+    }
     componentDidUpdate(prevProps, prevState) {
-        console.log("this.props.geometry:",this.props.geometry);
-        console.log("this.props.surface:",this.props.surface);
-        console.log("this.props.light:",this.props.light);
+        // console.log("this.props.geometry:",this.props.geometry);
+        // console.log("this.props.surface:",this.props.surface);
+        // console.log("this.props.light:",this.props.light);
+        console.log("this.props.initialDatState",this.props.data.string)
+        
         this.scene = new THREE.Scene();
+        var plane = getPlane(20);
+        plane.rotation.x = Math.PI/2;
+        this.scene.add(plane);
+        var sphere = getSphere(0.05);
         var size = 1
 		var segmentMultiplier = 1;
         var geometry
@@ -59,7 +144,7 @@ class Scene extends Component {
 				break;
             case 'cylinder':
                 console.log("this.props.children:",this.props.children); 
-                geometry = new THREE.CylinderGeometry(5, 5, 20, 32);
+                geometry = new THREE.CylinderGeometry(1, 1, 4, 10);
                 break;
 			default:
 				break;
@@ -70,7 +155,7 @@ class Scene extends Component {
 				break;
 			case 'line':
 				material = new THREE.MeshNormalMaterial({
-                    color: 0xff0000,
+                    color: this.props.data.colorObject,
                     transparent: true,
                     opacity: 1,
                     wireframe: true,
@@ -81,21 +166,22 @@ class Scene extends Component {
 				break;
 			case 'solid':
 				material = new THREE.MeshPhongMaterial({
-                    color: 'rgb(0, 0, 0)'
+                    color: this.props.data.colorObject
                 });
 				break;
 			default:
+                material = new THREE.MeshBasicMaterial({ color: this.props.data.colorObject });
 				break;
 		}
         switch (this.props.light) {
 			case 'point':
-				light = new THREE.PointLight( 0xffffff, 30);
+				light = new THREE.PointLight( 0xffffff, 1);
                 // light.position.set( 50, 50, 50 );
-                light.position.y = 3;
-                
+                light.position.y = this.props.data.lightPosition;
+                light.add(sphere);
 				break;
 			case 'ambient':
-				light = new THREE.AmbientLight( 0x404040 );;
+				light = new THREE.AmbientLight( 0x404040 );
 				break;
 			case 'remove':
 				geometry = new THREE.ConeGeometry(size, size, 256*segmentMultiplier);
@@ -104,12 +190,30 @@ class Scene extends Component {
 				break;
 		}
         this.cube = new THREE.Mesh(geometry, material );
-
+        
+        // camera.position.z = 4;
+        this.camera.fov = this.props.data.fov
+        this.camera.near = this.props.data.near
+        this.camera.far = this.props.data.far
+        this.camera.updateProjectionMatrix();
         this.scene.add(this.cube);
         this.scene.add( light );
-        this.renderer.render(this.scene, this.camera);
+        this.scene.add( this.gridHelper );
+        
+        // this.scene.add(transformControls);
+        // renderer.setClearColor("#000000");
+        // renderer.setSize(width, height);
 
-        this.start();
+        // this.scene = scene;
+        // this.camera = camera;
+        // this.renderer = renderer;
+        // this.material = material;
+        // this.cube = cube;
+        this.renderer.render(this.scene, this.camera);
+        
+        this.renderer.setClearColor('rgb(120, 120, 120)');
+        // this.mount.appendChild(this.renderer.domElement);
+        // this.start();
     }
 
     componentWillUnmount() {
@@ -129,8 +233,8 @@ class Scene extends Component {
     }
 
     animate() {
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
+        // this.cube.rotation.x += 0.01;
+        // this.cube.rotation.y += 0.01;
 
         this.renderScene();
         this.frameId = window.requestAnimationFrame(this.animate);
@@ -138,6 +242,7 @@ class Scene extends Component {
 
     renderScene() {
         this.renderer.render(this.scene, this.camera);
+        // this.controls.update();
     }
 
     render() {
@@ -153,3 +258,4 @@ class Scene extends Component {
 }
 
 export default Scene;
+
